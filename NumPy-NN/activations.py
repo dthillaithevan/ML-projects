440db034567555e4845bfa0b2fc5931b2f86dbf6
Dee718 <dt718@ic.ac.uk>
2025-02-19 22:38:41 +0000

    Adding Sigmoid

---
+++ NumPy-NN/activations.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""
+Created on Wed Feb 19 17:14:18 2025
+
+@author: Dilaksan Thillaithevan
+"""
+import numpy as np
+from nn_module import Module
+
+
+class Relu(Module):
+    """Relu activation layer: a = max(0, z)"""
+
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__(name="ReLu")
+
+        # Placeholder for input & gradients
+        self.x = None
+        self.grad_a = None
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """a = max(0, x)"""
+        self.x = x
+        self.a = np.maximum(0, x)
+        return self.a
+
+    def backprop(self, grad_output: np.ndarray) -> np.ndarray:
+        """ """
+        self.grad_a = (self.x > 0).astype(self.x.dtype) * grad_output
+        return self.grad_a
+
+
+class Identity(Module):
+    """Identity activation: a = x"""
+
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__(name="Identity")
+
+        # Placeholder for input & gradients
+        self.x = None
+        self.grad_a = None
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """a = x"""
+        self.x = x
+        self.a = x
+        return self.a
+
+    def backprop(self, grad_output: np.ndarray) -> np.ndarray:
+        """grad = Identity"""
+        # self.grad_a = np.ones_like(grad_output, dtype = self.x.dtype)
+        return grad_output
+
+
+class Sigmoid(Module):
+    """Sigmoid activation layer: a = 1/(1 + e^-z)"""
+
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__(name="Sigmoid")
+
+        # Placeholder for input & gradients
+        self.x = None
+        self.grad_a = None
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """a = 1/(1 + e^-x)"""
+        self.x = x
+        self.a = 1 / (1 + np.exp(-x))
+        return self.a
+
+    def backprop(self, grad_output: np.ndarray) -> np.ndarray:
+        """d(sig)/dx = sig(x) * (1-sig(x))"""
+        self.grad_a = self.a * (1 - self.a) * grad_output
+        return self.grad_a
+
+
+DEFAULT_ACTIVATION = Relu
+ACTIVATIONS = {
+    "ReLu": Relu,
+    None: DEFAULT_ACTIVATION,
+    "Identity": Identity,
+    "Sigmoid": Sigmoid,
+}
