e1ef2336853c255a0e032a11050b812722baaf4a
Dee718 <dt718@ic.ac.uk>
2025-02-24 17:56:39 +0000

    Update example_ce_classification.py

--- NumPy-NN/example_ce_classification.py
+++ NumPy-NN/example_ce_classification.py
@@ -11,10 +11,17 @@ from nn import NN
 from optimiser import OPTIMIZERS
 from loss import CrossEntropy
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 from utils import one_hot_encode
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
+    max_iters = 10000
+    loss_tol = 1e-04
+    print_iters = 100
+    batch_size = 105
+    train_size = 0.7
 
     # =============================================================================
     # Load data
@@ -26,7 +33,9 @@ if __name__ == "__main__":
     X_names = iris["feature_names"]
     Y_names = iris["target_names"]
     Y = one_hot_encode(Y, len(Y_names))
-
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, train_size=train_size, shuffle=True
+    )
     # =============================================================================
     # Define NN params
     # =============================================================================
@@ -41,23 +50,38 @@ if __name__ == "__main__":
     nn = NN(layer_sizes, activations)
     ce = CrossEntropy()
 
-    max_iters = 1000
-    loss_tol = 1e-04
-    print_iters = 100
-
     lr = 0.1
     adam = OPTIMIZERS["Adam"](nn, lr)
+    hist = []
 
-    for it in range(max_iters):
-        Y_hat = nn(X)
-        loss = ce(Y_hat, Y)
-        if loss <= loss_tol:
-            print("-" * 20, f"Converged after {it} iterations", "-" * 20, flush=True)
-            break
-        else:
-            if it % print_iters == 0:
-                print(f"Iteration {it} out of {max_iters}", flush=True)
-                print(f"\tLoss: {loss:.2f}", flush=True)
+    it = 0
+    conv = False
+    while it < max_iters and not conv:
+        for batch in range(0, len(X_train), batch_size):
+            end = min(batch + batch_size, len(X_train))
+            X = X_train[batch:end]
+            Y = Y_train[batch:end]
 
-            nn.backprop(ce)
-            adam.step()
+            Y_hat_test = nn(X_test)
+            test_loss = ce(Y_hat_test, Y_test)
+
+            if test_loss <= loss_tol:
+                conv = True
+                print(
+                    "-" * 20, f"Converged after {it} iterations", "-" * 20, flush=True
+                )
+            else:
+                if it % print_iters == 0:
+                    print(f"Iteration {it} out of {max_iters}", flush=True)
+                    print(f"\tLoss: {test_loss:.2f}", flush=True)
+                Y_hat = nn(X)
+                loss = ce(Y_hat, Y)
+                nn.backprop(ce)
+                adam.step()
+                hist += [test_loss]
+
+        it += 1
+
+    plt.plot(hist)
+    plt.grid()
+    plt.show()
